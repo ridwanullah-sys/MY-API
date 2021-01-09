7e33dde88fe05604662de1c3e3e636fccc6e51f9
ridwanullah <abdraufridwantemi@gmail.com>
2021-01-09 01:55:06 -0800

    adding more field

--- app/Http/Resources/product/ProductResource.php
+++ app/Http/Resources/product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'total price' => round((1-($this->discount/100)) * $this->price,2),
+            'rating' => $this->review->count() > 0 ? round($this->review->sum('star')/$this->review->count(),2) : 'no rating yet',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
